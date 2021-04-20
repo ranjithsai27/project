18dc4b4c9e207de1925f96bc53628025f77ee2c6
ranjith <ranjithsai446@gmail.com>
2021-04-20 16:44:47 +0530

    table

--- my app/src/App.js
+++ my app/src/App.js
@@ -1,81 +1,115 @@
-import "./App.css";
-import React from 'react';
-import { BasicTable } from "./Components/basictable/BasicTable";
+import './App.css';
+import axios from "axios";
+import React, { Component, useEffect, useState } from "react";
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function App (){
-  return(
-    <div className='App'>
-      <BasicTable />
-    </div>
-  )
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+const useStyless = {
+  backgroundColor: "orangered",
+  color: "white"
 }
 
+const backstyle ={ 
+  
+    backgroundColor: "black",
+    color: "white",
+    fontSize : "large",
+    fontFamily : "italic"
+  
+}
 
+ const styless ={
+  fontSize : "large",
+  fontFamily : "italic"
+ }
 
+  const mystyle={
+    color: "black",
+  backgroundColor: "white",
+  padding: "-10px",
+  fontFamily: "italic",
+  float : "right"
+  }
 
+const App = () => {
+  const classes = useStyles();
+  const [product, setProduct] = useState([]);
+  const [search, setSearch] = useState("");
 
+  const getProductData = async () => {
+    try {
+      const data = await axios.get("https://jsonplaceholder.typicode.com/users"
+      );
+      console.log(data.data);
+      setProduct(data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
+  useEffect(() => {
+    getProductData();
+  }, []);
 
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search here" style={mystyle}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead >
+            <TableRow>
+              <TableCell style={backstyle}>Name</TableCell>
+              <TableCell style={backstyle}>Email Id</TableCell>
+              <TableCell style={backstyle} >Phone</TableCell>
+              <TableCell style={backstyle}>City</TableCell>
+              <TableCell style={backstyle}>CompanyName</TableCell>
+              <TableCell style={backstyle}>More Info Links</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {product.filter((item) => {
+              if (search == "") {
+                return item;
+              } else if (
+                item.name.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return item;
+              }
+            })
+              .map((item) => (
+                <TableRow key={item.name}  >
+                  <TableCell component="th" scope="item" style={styless}>{item.name} </TableCell>
+                  <TableCell component="th" scope="item" style={styless}>{item.email}</TableCell>
+                  <TableCell component="th" scope="item" style={styless}>{item.phone}</TableCell>
+                  <TableCell component="th" scope="item" style={styless}>{item.address.city}</TableCell>
+                  <TableCell component="th" scope="item" style={styless}>{item.company.name}</TableCell>
+                  <TableCell component="th" scope="item" style={styless}> <button style={useStyless} >More Info</button></TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from "axios";
-// import React, { Component, getData,useEffect } from "react";
-
-// const App = () => {
-//   const getData = async () => {
-//     await axios.get('https://en.wikipedia.org/w/api.php', {
-//       params: {
-//         action: "query",
-//         list: "search",
-//         origin: "*",
-//         format: "json",
-//         srsearch: "html"
-//       }
-//     });
-//     console.log(getData);
-//   }
-// };
-
-// useEffect(() => {
-//   getData();
-// }, []);
-
-// const App =() =>{
-//   return(
-//   <div className="App">
-//     <h1>hello world</h1>
-//   </div>
-//   )
-//   };
-
-
-
-
-
+}
 
 export default App;
